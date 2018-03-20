abfecaaeeaf1c57cea38b083f00897a938c3a157
wuleying <5590548@qq.com>
2018-03-20 14:26:26 +0800

    Update

--- trader/__init__.py
+++ trader/__init__.py
@@ -1,2 +1,4 @@
+from .api import *
+
 __version__ = '0.0.1'
 __author__ = 'luoliang1'
--- trader/main.py
+++ trader/main.py
@@ -1,7 +1,11 @@
 # coding:utf8
+import sys
+
+if sys.version_info[:2] < (3, 6):
+    raise Exception('Python版本需要3.5或以上, 请升级Python.')
 
 def main():
-    print("hello")
+    print("hello world.")
 
 
 if __name__ == '__main__':
