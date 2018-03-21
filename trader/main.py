2b15fb8c301d3e5b27381ecf5345a59a8519a3c3
wuleying <5590548@qq.com>
2018-03-21 16:20:05 +0800

    Update.

--- trader/main.py
+++ trader/main.py
@@ -1,11 +1,16 @@
 # coding:utf8
 import sys
 
-if sys.version_info[:2] < (3, 6):
+import easyquotation
+
+if sys.version_info[:2] < (3, 5):
     raise Exception('Python版本需要3.5或以上, 请升级Python.')
 
+
 def main():
-    print("hello world.")
+    quotation = easyquotation.use('sina')
+    data = quotation.real('162411')
+    print(data)
 
 
 if __name__ == '__main__':
